15b8d66a5c0288e65e97462cc8bde07b1010a577
ceojs80 <ceojs80@naver.com>
2024-11-12 21:31:11 +0900

    오늘 배운 자바

---
+++ 자바241112.java
@@ -0,0 +1,24 @@
+package java_study;
+
+// 자바 공부를 시작하겠습니다. (메모 <- 주석)
+public class 자바241112 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println();
+		System.out.println("안녕하세요");
+		//syso
+		//syso ("현재 년도는 : + 2024 + "입니다.")
+		//syso ("키 : + 165 + "입니다.")
+		//syso ("나이 : + 44 + "입니다.")
+		System.out.print("안녕하세요");
+		System.out.print("안녕하세요");
+		
+		//변수
+		int age = 20; //나이
+		String address = "대전 서구 둔산동"; //주소
+		String mbti = "ESFJ" ;		
+	    System.out.println("나이는 : " + age);		
+		
+	}
+}
---
+++ 자바복습.java
@@ -0,0 +1,24 @@
+package java_study;
+
+import java.util.Scanner;
+
+public class 자바복습 {
+
+	public static void main(String[] args) {
+		// String name = "홍길동" ; //이름
+		int age = 20; //나이
+		String address = "대전 서구 둔산동"; 
+		String hobby = "운동";
+		String mbti = "esfj";
+		
+		
+		
+		
+	
+				
+		//입력받기
+	    Scanner scan = new Scanner(System.in);
+		
+	}
+
+}
