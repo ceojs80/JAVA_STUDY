51125eb7375b4c539fa999b10e2834a034ce0d9f
ceojs80 <ceojs80@naver.com>
2024-11-21 21:32:03 +0900

    Add files via upload

---
+++ java_study_1121/문제.java
@@ -0,0 +1,10 @@
+package java_study_1121;
+
+public class 문제 {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
---
+++ java_study_1121/수업1.java
@@ -0,0 +1,27 @@
+package java_study_1121;
+
+import java.util.Scanner; // 스캐너를 사용하기 위해 불러옴
+
+public class 수업1 {
+
+	public static void main(String[] args) {
+		
+		 //예제 => 시험 점수가 80점이 이상이면 합격 판별을 하는 프로그램을 작성하시오.
+		 Scanner scan = new Scanner(System.in); //ctrl + shift + 0
+		 
+		 System.out.print("점수를 입력하세요 :");
+		 //자바는 한글지원 가능!
+		 int 점수 = scan.nextlnt();
+		 System.out.println("입력한 점수는" + 점수);
+		 //
+		 
+		 
+		 
+		 //else 단독적으로 사용할 수 없습니다.
+		 //else는 if다음에 작성할 수 있습니다.
+
+		 
+		 //중점 if문 : 조건이 여러가지일 때 사용함
+	}
+
+}
