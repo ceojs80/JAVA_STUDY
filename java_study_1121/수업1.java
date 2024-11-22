f1be0bec8c49a0a39b9e172db01f6ff8ad0b3fed
ceojs80 <ceojs80@naver.com>
2024-11-22 20:47:47 +0900

    Add files via upload

--- java_study_1121/수업1.java
+++ java_study_1121/수업1.java
@@ -13,15 +13,38 @@ public class 수업1 {
 		 //자바는 한글지원 가능!
 		 int 점수 = scan.nextlnt();
 		 System.out.println("입력한 점수는" + 점수);
-		 //
-		 
-		 
-		 
+		 // >= : 비교 연산자
+		 if(점수 >=80) {//만약에 입력받은 점수가 80이상이라면?
+			 //true면 if 코드가 실행됨
+			 System.out.println("시험에 합격했습니다!");
+		 } else {
+			 //false면 else코드가 실행됨
+			 System.out.println("시험 불합격!");
+		 }
 		 //else 단독적으로 사용할 수 없습니다.
-		 //else는 if다음에 작성할 수 있습니다.
-
+		 //else는 if다음에 작성할 수 있습니다.(옵션)
+		 /*
+		 else{
 		 
+		 
+		 }
+		 */
 		 //중점 if문 : 조건이 여러가지일 때 사용함
-	}
+		 System.out.print("점수2를 입력하세요:");
+		 int 점수2 = scan.nextlnt();
+		 //&&:and 연산자(두 조건이 모두 true이어야 함)
+		 if(점수2>=90 && 점수2<=100) {//점수가 90이상 100이하라면...
+			 System.out.println("A등급");
+		 }else if(점수2<90 && 점수2>=80) { //점수가 90미만 80이상이라면
+			 System.out.println("B등급");
+		 }else if(점수2<80 && 점수2>=70) { //점수가 80미만 70이상이라면
+		     System.out.println("C등급");
+	     }else {
+	    	 System.out.println("F등급");
+	     }
+		 
+		 
 
+     }
+	
 }
